42940e6cc0b7602fbe19fa64c9b4e66e71ab9875
Anwar3006 <anwarsadat.d2@gmail.com>
2022-11-08 10:45:53 +0000

    Sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,28 +2,31 @@
 
 /**
  * bubble_sort - Algorithm rearranges array by swapping two out-of-place
- *               elements
- * @array: array to sort 
+ * elements
+ * @array: array to sort
  * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
 	bool swapped = false;
+
 	int i = 0;
-	do
-	{
-	swapped = false;
-	for (int j = 0; j < (size - 1 - i); j++)
-	{
-		if (array[j] > array[j + 1])
+	do {
+		swapped = false;
+
+		for (int j = 0; j < (size - 1 - i); j++)
 		{
-		int temp = array[j + 1];
-		array[j + 1] = array[j];
-		array[j] = temp;
-		print_array(array, size);
-		swapped = true;
+			if (array[j] > array[j + 1])
+			{
+
+				int temp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = temp;
+
+				print_array(array, size);
+				swapped = true;
+			}
 		}
-	}
 	i++;
 	} while (swapped);
 }
